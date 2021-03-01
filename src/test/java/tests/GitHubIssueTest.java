34ef4c12abebb28bf30052630c64dacf8e5ac0db
YakunichkinAV <yakunichkinav@multibonus.ru>
2021-03-01 23:24:32 +0300

    init

---
+++ src/test/java/tests/GitHubIssueTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHubIssueTest {
+
+    private static String repo = "avyakunichkin/qa_guru_lesson5";
+
+    @Test
+    void gitHubIssueTest(){
+        open("");
+    }
+}
